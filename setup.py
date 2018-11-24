15ad2857b59761d8bf692d4f283f1299b1ac2235
Sebastian Jüngling <s0558556@htw-berlin.de>
2018-11-24 14:35:31 +0100

    init commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="spacy_german_preprocess",
+    version="0.0.1",
+    author="Sebastian Jüngling",
+    author_email="author@example.com",
+    description="A small package for preprocessing german text",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
